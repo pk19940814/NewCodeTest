592165ed7afb3566562358a9f332da82a5ac3a8c
pengkun <kun.peng02@ele.me>
2017-11-01 20:11:37 +0800

    增加测试答案

--- src/LeetCode/SortList/Solution.java
+++ src/LeetCode/SortList/Solution.java
@@ -8,24 +8,61 @@ import java.util.List;
 public class Solution {
     // private ListNode result = null;
 
+    public static void main(String[] args) {
+        ListNode p1 = new ListNode(2);
+        ListNode p2 = new ListNode(5);
+        ListNode p3 = new ListNode(3);
+        ListNode p4 = new ListNode(1);
+        p1.next = p2;
+        p2.next = p3;
+        p3.next = p4;
+        ListNode p = new Solution().sortList(p1);
+        while (p != null) {
+            System.out.println(p.val);
+            p = p.next;
+        }
+    }
+
+
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) return head;
-
 
         return sort(head, null);
 
     }
 
-    public ListNode sort(ListNode p1, ListNode p2) {
-       ListNode head=new ListNode(-1);
-        ListNode p=p1.next;
-        while (p!=null){
-            if (p.val<p1.val){
-            }
+    public ListNode sort(ListNode head, ListNode tail) {
+        if (head == null || head.equals(tail)) {
+            return null;
         }
-       return head;
-    }
+        ListNode leftHead = new ListNode(-1);
+        ListNode leftPr = leftHead;
+        ListNode rightHead = new ListNode(-1);
+        ListNode rightPr = rightHead;
+        ListNode p = head.next;
+        ListNode pre = head;
 
+        while (pre != null && p != null) {
+            if (p.val < head.val) {
+                leftPr.next = p;
+                leftPr = leftPr.next;
+            } else {
+                rightPr.next = p;
+                rightPr = rightPr.next;
+            }
+            p = p.next;
+            pre = pre.next;
+
+        }
+
+        leftPr.next = head;
+        head.next = rightPr.next;
+
+        ListNode result = sort(leftHead.next, head);
+        sort(rightHead.next, null);
+        return result;
+
+    }
 
 }
 
