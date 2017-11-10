c37d04553c455ef5d033c2020a210bbb05c02465
pengkun <kun.peng02@ele.me>
2017-11-10 17:37:04 +0800

    增加测试答案

--- src/SchoolRecruit2016/XIAOMI/ReverseSentence/Main.java
+++ src/SchoolRecruit2016/XIAOMI/ReverseSentence/Main.java
@@ -1,9 +1,22 @@
 package SchoolRecruit2016.XIAOMI.ReverseSentence;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        while (in.hasNext()) {
+            list.add(in.next());
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i));
+            if (i != 0) {
+                System.out.print(" ");
+            }
+        }
     }
 }
